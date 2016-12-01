1584954631668f670ad20e29520b71b52f94c4e3
Nigel Karan <Nigel@Nigels-iMac.local>
2016-12-01 14:10:30 +0100

    Completed setting up Firebase

---
+++ MashBinSocial/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MashBinSocial
+//
+//  Created by Nigel Karan on 01/12/16.
+//  Copyright © 2016 EatMyGoal. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
